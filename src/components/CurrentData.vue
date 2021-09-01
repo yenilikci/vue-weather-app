594456fd8bc91d6fc06b859a6ffc894c72ebbc22
yenilikci <m.melihcelik017@outlook.com>
2021-09-01 14:15:42 +0300

    Add vue-resource and Create CurrentData component

---
+++ src/components/CurrentData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="currentValue()">Get weather</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      currentValue() {
+        this.$http
+          .get(
+            "http://localhost:8080/data/2.5/weather?q=London&appid=e27e11e0f84ba08de34ebcccbdc01163"
+          )
+          .then(
+            (response) => {
+              console.log(response.body.weather);
+            },
+            (response) => {
+              console.log(response);
+            }
+          );
+      },
+    },
+  };
+</script>
+
+<style></style>
